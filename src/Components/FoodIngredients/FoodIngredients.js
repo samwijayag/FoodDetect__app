97d48142a0979e1c03f5ab19fd5a4e1e38b9d171
samwijayag <sam.wijayag@gmail.com>
2019-03-09 21:43:21 -0800

    initial commit

---
+++ src/Components/FoodIngredients/FoodIngredients.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FoodIngredients = () => {
+	return(
+		<div className='center ma'>
+			<div className='absolute mt2'>
+				<img alt=""
+				 src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwArCTOd8GLjeEGDrpanPxMqvZgyCZvjN8RdCwo99g97ojGHgI' 
+				 width='400px'
+				 height='auto'
+				  />
+			</div>
+		</div>
+	)
+}
+
+export default FoodIngredients;
