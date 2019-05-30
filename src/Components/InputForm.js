6b63c9238ebb89e9c73903847bc64c18194882ce
samwijayag <sam.wijayag@gmail.com>
2019-05-29 21:03:46 -0700

    bug fixes

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -7,14 +7,14 @@ const InputForm = ({onInputChange, onInputSubmit}) => {
 			<h1 className='f2 fw7 ttu tracked lh-title mt0 mb3 avenir shadow-4 pa3 bg-light-gray'>FOOD DETECT</h1>
 			<h2 className='f3 mid-gray lh-title'>This App detects food items and ingredients in Images of food. Give it a try</h2>
 			<div className='center'>
-				<div className='center w-40 pa4 br3 shadow-5'>
+				<div className='center w-40 pa3 br3 shadow-5'>
 					<input 
 						onChange={onInputChange}
-						className='f3 pa2 w-70 center tc'
+						className='f3 pa2 w-80 center tc'
 						placeholder='submit url'
 						type='text' /> 
 					<button 
-						className='w-30 grow bf4 link ph3 pv2 dib white bg-light-red'
+						className='w-30 grow ma1 bf4 link ph3 pv2 dib white bg-light-red'
 						onClick={onInputSubmit}
 						>
 						Detect
--- src/Components/Particles.js
+++ src/Components/Particles.js
@@ -1,17 +1,7 @@
-import React, {Component} from 'react';
-import InputForm from './Components/InputForm'
-import OutputForm from './Components/OutputForm'
-import IngredientList from './Components/IngredientList'
-import './App.css';
-import Clarifai from 'clarifai';
+import React from 'react'
 import Particles from 'react-particles-js';
 
-const app = new Clarifai.App({
- apiKey: 'e4c93404d9be4739ae6b173d98ca7a08'
-});
-
-
-const particlesOptions = {
+const Particles = () => {
     particles: {
       number: {
         value: 9,
@@ -122,51 +112,4 @@ const particlesOptions = {
       retina_detect: true
 }
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      foodItems: [],
-    }
-  }
-
-  foodIngredients = (data) => {
-    const clarifaiFood = data.outputs[0].data.concepts
-    this.setState({foodItems: clarifaiFood})
-  }
-
-  onInputChange = (event) => {
-    this.setState({input: event.target.value})
-  }
-
-  onInputSubmit = () => {
-      this.setState({imageUrl: this.state.input})
-      app.models.predict(
-        Clarifai.FOOD_MODEL,
-        this.state.input)
-      .then(response => this.foodIngredients(response))
-      .catch(err => console.log(err))
-  }
-
-  render(){
-    return (
-      <div className="App">
-       <Particles className='particles'
-          params={particlesOptions}
-        />
-      <InputForm 
-        onInputChange={this.onInputChange}
-        onInputSubmit={this.onInputSubmit}
-        />
-      <OutputForm 
-        imageUrl={this.state.imageUrl}
-        />
-      <IngredientList foodItems = {this.state.foodItems}/>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default Particles
